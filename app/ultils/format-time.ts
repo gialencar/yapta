b642dc1ef04c7bda00d64dc00f01266b2a7cc308
Gilson Alencar <gilson.alencar94@gmail.com>
2024-02-12 17:27:43 -0300

    feat: add formatTime utility function

---
+++ app/ultils/format-time.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Formats the given time in seconds into a string representation of minutes and seconds.
+ * @param time - The time in seconds to format.
+ * @returns The formatted time string in the format "mm:ss".
+ */
+export const formatTime = (time: number) => {
+  let minutes = Math.floor(time / 60).toString().padStart(2, '0');
+  let seconds = (time % 60).toString().padStart(2, '0');
+
+  return `${minutes}:${seconds}`
+};
